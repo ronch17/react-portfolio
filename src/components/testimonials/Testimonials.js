3c5f1d57497ba4780fe4285f158dce32c13496e9
Ron Dayan <rondayan@Rons-MacBook-Pro.local>
2025-10-08 23:55:50 +0300

    Updated testimonials

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -9,16 +9,21 @@ import "swiper/css/pagination";
 
 const dummyData = [
   {
-    avatar: "Avt1",
+    name: "Ronit Vinocur",
+    jobTitle: "QA Engineer",
+    review:
+      "It has been a pleasure working with Ron. As a Fullstack Developer, he has a strong ability to build complete solutions, moving seamlessly across the application while always maintaining high quality. What I truly appreciate is that he was never defensive when receiving QA feedback—he listened carefully, was open to improvements, and actively collaborated to ensure the best outcome for the product. His professionalism, technical expertise, and team spirit make him an invaluable member of any project",
+  },
+  {
     name: "Or Malchi",
+    jobTitle: "Website customer",
     review:
       "Ron is a true professional, he helped me build my website in the most professional way I've ever seen. He was free and available to me and my wishes throughout the entire work with him, and offered great suggestions for improvement, highly recommend!",
   },
   {
-    avatar: "Avt2",
     name: "Shachar Press",
-    review:
-      `Working with Ron is very pleasant, available and able to provide solutions
+    jobTitle: "Website customer",
+    review: `Working with Ron is very pleasant, available and able to provide solutions
       Even after several months he sent me a screenshot of an action I forgot how to do
       He has a wide knowledge of website development and even when I needed something complicated that I wanted, he searched for it and implemented it on the website.
       My site in terms of Google optimization is good and it is constantly progressing even when I don't invest in it.
@@ -27,11 +32,11 @@ const dummyData = [
   },
 ];
 
-const articleGeneretor = dummyData.map(({ avatar, name, review }, index) => {
+const articleGeneretor = dummyData.map(({ jobTitle, name, review }, index) => {
   return (
     <SwiperSlide key={index} className="testimonial">
-
       <h5 className="client__name">{name}</h5>
+      <h6 className="client__job">{jobTitle}</h6>
       <small className="client__review">{review}</small>
     </SwiperSlide>
   );
